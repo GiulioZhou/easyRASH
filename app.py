44193751e51c6ef03428ec393a601379603558ef
Giulio <giuliozhou@gmail.com>
2016-05-03 23:36:51 +0200

    add main page with toolbar and sidenav

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+    
+@app.route('/home', methods=['GET', 'POST'])
+def base():
+    return render_template('base.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,14 @@
  var app = angular.module('myApp', ['ngMaterial'])
+ 
  	.config(function($mdThemingProvider) {
  		$mdThemingProvider.theme('default')
  			.primaryPalette('blue-grey')
  			.accentPalette('red');
-});
+	});
+	
+
+app.controller('MyController', function($scope, $mdSidenav) {
+  $scope.openLeftMenu = function() {
+    $mdSidenav('left').toggle();
+  };
+  });
