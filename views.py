712002c01cdbaf104ccf4dcf09e1831e5b31bfc9
Giulio Zhou <Giulio@MBP-di-Giulio.station>
2016-07-29 17:43:18 +0200

    view

--- views.py
+++ views.py
@@ -1,2 +1,9 @@
 from easyRASH import app
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	return render_template('index.html')
+
+@app.route('/home', methods=['GET', 'POST'])
+def base():
+	return render_template('base.html')
