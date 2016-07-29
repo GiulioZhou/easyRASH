c5939a106a4d83595b26d59aeb7e232352563656
Giulio Zhou <Giulio@MBP-di-Giulio.station>
2016-07-29 17:47:31 +0200

    view

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Response, request, render_template, send_from_directory
+from flask import Flask, render_template
 from easyRASH import app
 
 @app.route('/', methods=['GET', 'POST'])
